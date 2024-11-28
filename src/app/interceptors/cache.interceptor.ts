04ea2739ec9796822752978b69adf9a13cc7e62e
Noé Pécault <noe.pecault@soprasteria.com>
2024-11-28 11:26:08 +0100

    feat(cache): Added NoCacheToken for allowing uncached requests

--- src/app/interceptors/cache.interceptor.ts
+++ src/app/interceptors/cache.interceptor.ts
@@ -1,11 +1,19 @@
-import {HttpInterceptorFn, HttpResponse} from '@angular/common/http';
+import {HttpContextToken, HttpInterceptorFn, HttpResponse} from '@angular/common/http';
 import {inject} from '@angular/core';
 import {CacheService} from '../services/cache/cache.service';
 import {filter} from 'rxjs/operators';
 
+export const NoCacheToken = new HttpContextToken<boolean>(() => false);
+
 export const cacheInterceptor: HttpInterceptorFn = (req, next) => {
   const cache = inject(CacheService);
 
+  if (req.context.get(NoCacheToken)) {
+    return next(req);
+  }
+
+  // Cached HTTP responses are not instances of HttpResponse but only objects.
+  // We need to transform back cached responses to instances of the class.
   return cache.match<{ [key in keyof HttpResponse<unknown>]: HttpResponse<unknown>[key] }, HttpResponse<unknown>>(
     req.url,
     next(req).pipe(filter(event => event instanceof HttpResponse)),
